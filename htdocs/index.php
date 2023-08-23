52a12b5a413cb8460fd7fe0c330110e87f49a572
soslan <so.gabarayev@gmail.com>
2023-08-23 11:19:20 +0200

    Ajout de template

---
+++ htdocs/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>  <html class="no-js ie ie6 lt-ie10 lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>     <html class="no-js ie ie7 lt-ie10 lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>     <html class="no-js ie ie8 lt-ie10 lt-ie9" lang="en"> <![endif]-->
+<!--[if IE 9]>     <html class="no-js ie ie9 lt-ie10" lang="en"> <![endif]-->
+<!--[if IE 10]>    <html class="no-js ie ie10" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Zoo</title>
+    <meta name="description" content="Zoo Atlanta">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/min/script-min.js"></script>
+  </head>
+
+  <body class="green-bg">
+    <header role="banner">
+      <div class="container clearfix">
+        <div class="hero">
+                <span>                
+                    <svg viewBox="0 0 1320 300">
+                        <text x="50%" y="50%" dy=".35em" text-anchor="middle">
+                            sooozooo
+                        </text>
+                    </svg>	
+                </span>
+          <div class="hero-content-wrap">
+            <div class="load-animal left"><img src="assets/imgs/tiger.png" class="tossing" alt=""></div>
+            <a href="map.html" class="load-button left bounce">Allons Explorer!
+              <img src="assets/imgs/load-button.png" class="tiger" alt="">
+            </a>
+            <div class="load-animal right"><img src="assets/imgs/panda.png" class="tossing" alt=""></div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </body>
+</html>
