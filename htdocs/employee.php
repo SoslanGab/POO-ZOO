fbf1e37be06b0572039d49c3c5a6bbab4d6de474
SoslanGab <so.gabarayev@gmail.com>
2023-08-29 10:02:54 +0200

    creations de nav-bar + fichier emlpoyee

---
+++ htdocs/employee.php
@@ -0,0 +1,5 @@
+<?php include 'linksscripts.php'; ?>
+
+
+
+
--- htdocs/index.php
+++ htdocs/index.php
@@ -3,13 +3,30 @@
 
   <body class="green-bg">
     <navbar class="">
-        <nav class="navbar bg-dark navbar-dark">
-            <div class="container-fluid">
-              <a href="">
-                <i class="fa-solid fa-screwdriver-wrench" style="color: #c76305;">Admin</i>
-              </a>
-            </div>
-        </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Mon Zoo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Animaux</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Enclos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Employés</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     </navbar>
     <header role="banner">
       <div class="container clearfix">
